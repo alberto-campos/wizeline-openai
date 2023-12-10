7e9062f4601e0d05216da2389d577cafccfbb99b
Alberto Campos <acampos@slack-corp.com>
2023-12-09 22:50:44 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # wizeline-openai
 OpenAI Wizeline Sprint December 2023
+
+# WordWize Refiner
+Do you ever feel inspired and creative, really wanting to express your ideas, but sense the need to slow down or suppress thoughts just because you fear people won't understand your accent? Well, who cares! Just say it anyway and we'll take care of the rest!
+
+# App's Purpose
+Our OpenAI tool analyzes transcripts from Spanish-speaking users and recommends a more natural English version that emphasizes native language usage and semantics. Additionally, it highlights common mistakes, providing a brief explanation along with a recommendation.
+
+
+# Installation and Requirements
+Make sure you have a relatively recent Python version. For testing purposes, we don't need the latest.
+
+`python --version`
+
+Sample output:
+```
+Python 3.10.4
+```
+
+Clone this repository
+`gh repo clone alberto-campos/wizeline-openai`
+
+Create an OPENAI_API_KEY key
+`echo "export OPENAI_API_KEY='yourkey'" >> ~/.zshrc`
+
+`source ~/.zshrc`
+
+Confirm your environment key was setup correctly
+`echo $OPENAI_API_KEY`
+
+Navigate to the `capstone_project` directory
+
+Install required packages
+`pip install -r requirements.txt`
+
+Create a Python virtual environment
+`python3 -m venv .venv`
+
+Activate this recently craated environment
+`source .venv/bin/activate`
+
+Install Jupyter
+`pip install notebook ipywidgets`
+
+Open Jupyter
+`jupyter notebook`
+
+Import packages
+`pip install numpy openai`
+
+Run the app:
+`python main.py`
+
+## Optional/Troubleshooting
+You may need to upgrade openAI
+`openai migrate`
---
+++ capstone_project/brain_module.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+import os
+from openai import OpenAI
+
+client = OpenAI(api_key=self.api_key)
+from dotenv import load_dotenv
+
+class ChatGPT:
+    """A class to interact with OpenAI's ChatGPT model."""
+
+    def __init__(self):
+        # Load environment variables from the .env file
+        load_dotenv()
+
+        # Retrieve the OPENAI_API_KEY environment variable
+        self.api_key = os.getenv("OPENAI_API_KEY")
+
+        # Set the retrieved API key for the OpenAI library
+
+        # A constant to describe the role or behavior of the chatbot
+        self.MAIN_ROLE = "This is the behavior of chatGPT"
+
+    def request_openai(self, message, role="system"):
+        """
+        Make a request to the OpenAI API.
+
+        Args:
+        - message (str): The message to be sent to the OpenAI API.
+        - role (str, optional): The role associated with the message. Defaults to "system".
+
+        Returns:
+        - str: The response content from the OpenAI API.
+        """
+
+        # Create a chat completion with the provided message and role
+        response = client.chat.completions.create(model="gpt-3.5-turbo",
+        messages=[{"role": role, "content": message}])
+
+        # Return the message content from the API response
+        return response["choices"][0]["message"]["content"]
+    
+    def analyze_transcription(transcription):
+        # OpenAI API call
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            messages=[
+                {"role": "system", "content": "You are a Spanish-speaking person learning English and want feedback on expressing yourself in natural English."},
+                {"role": "user", "content": transcription},
+            ]
+        )
+
+        # Extract the AI-generated response
+        ai_response = response.choices[0].message.content
+        
+        return ai_response
+    
+    def display_recommendations(user_input, ai_output):
+        display(HTML(f"<strong>User Input:</strong> {user_input}"))
+        display(HTML(f"<strong>AI Output:</strong> {ai_output}"))
+
+        # Example transcription from a Spanish-speaking user
+        user_transcription = """
+            Hello everyone, my name is Mr. Rodrigo Rodriguez.
+
+            Hope your doing well. How do you call that? I want to make sure because I don't want to do a mistake.
+
+            The bus is late, no? 
+
+            I guess is not good that the bus is always late.
+
+
+            """
+        # Analyze the transcription and get AI-generated recommendations
+        ai_recommendations = analyze_transcription(user_transcription)
+
+        # Display the results
+        display_recommendations(user_transcription, ai_recommendations)
+
+
+# If you need to test or use this directly, you can do:
+# if __name__ == "__main__":
+#     chat_gpt = ChatGPT()
+#     print(chat_gpt.request_openai("Hello!"))
